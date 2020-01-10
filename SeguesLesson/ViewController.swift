738f970c0e13ff0d78f246bbe3e19d488298010c
David Svensson <davidsvson@gmail.com>
2020-01-10 11:09:59 +0100

    Initial Commit

---
+++ SeguesLesson/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeguesLesson
+//
+//  Created by David Svensson on 2020-01-10.
+//  Copyright © 2020 David Svensson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
